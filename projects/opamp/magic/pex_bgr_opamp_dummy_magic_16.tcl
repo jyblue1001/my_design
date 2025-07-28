70338a519c59a5b6db204a642fa41fae94045783
Jae Young Yoon <jyblue1001@naver.com>
2025-07-28 14:29:47 +0900

    desktop37_0728

---
+++ projects/opamp/magic/pex_bgr_opamp_dummy_magic_16.tcl
@@ -0,0 +1,21 @@
+crashbackups stop
+drc off
+load bgr_opamp_dummy_magic_16.mag
+select top cell
+flatten bgr_opamp_dummy_magic_16_flat
+load bgr_opamp_dummy_magic_16_flat
+cellname delete bgr_opamp_dummy_magic_16
+cellname rename bgr_opamp_dummy_magic_16_flat bgr_opamp_dummy_magic_16
+select top cell
+extract path /foss/designs/my_design/projects/opamp/magic
+ext2spice lvs
+extract do resistance
+extract all
+ext2sim labels on
+ext2sim
+extresist tolerance 10
+extresist all
+ext2spice extresist on
+ext2spice cthresh 0.01
+ext2spice -p /foss/designs/my_design/projects/opamp/magic -o /foss/designs/my_design/projects/opamp/magic/bgr_opamp_dummy_magic_16.pex.spice.tmp
+quit -noprompt
